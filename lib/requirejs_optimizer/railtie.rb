e220f64d37ff67375d5c099aabcfb5ea8bef3c9b
Matt Wilson <mhw@hypomodern.com>
2013-06-13 15:48:38 -0400

    respect vendor/assets as well as app/assets
    Conflicts:
    	lib/requirejs_optimizer/railtie.rb

--- lib/requirejs_optimizer/railtie.rb
+++ lib/requirejs_optimizer/railtie.rb
@@ -11,25 +11,28 @@ class RequirejsOptimizerRailtie < Rails::Railtie
 
   config.before_initialize do
     Rails.application.config.assets.compress = false
-  end
 
-  config.after_initialize do
-    javascripts_root_path = Rails.root.join(*%w(app/assets/javascripts/))
-    modules_path          = javascripts_root_path.join(RequirejsOptimizer.base_folder, '**', '*.{coffee,js}')
-
-    modules = Dir[modules_path].reject { |f| f =~ /require\.build\.js$/ }.map do |path_with_filename|
-      filename = path_with_filename.gsub(/^#{javascripts_root_path}\/?/, '').gsub(/\.coffee$/, '')
-      filename = "#{filename}.js" unless File.extname(filename) == ".js"
-      filename
+    %w(vendor app).each do |location|
+      javascripts_root_path = Rails.root.join("#{location}/assets/javascripts/")
+      modules_path          = javascripts_root_path.join("authoring", '**', '*.{coffee,js}')
+      add_modules_from_path_to_precompile_list modules_path, javascripts_root_path
     end
-
-    Rails.application.config.assets.precompile += modules
   end
 
   rake_tasks do
     raketask = RequirejsOptimizer::Rake::Task.new
     raketask.define_tasks
     raketask.extend_default_precompile unless ENV['NO_RJS']
+  end
+
+  def add_modules_from_path_to_precompile_list modules_path, root_path
+    modules = Dir[modules_path].reject { |f| f =~ /require\.build\.js$/ }.map do |path_with_filename|
+      filename = path_with_filename.gsub(/^#{root_path}\/?/, '').gsub(/\.coffee$/, '')
+      filename = "#{filename}.js" unless File.extname(filename) == ".js"
+      filename
+    end
+
+    Rails.application.config.assets.precompile += modules
   end
 
 end
